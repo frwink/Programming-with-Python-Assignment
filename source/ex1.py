332958ba7f83a30104d0011eb528b572486ff85f
frwink <107881240+frwink@users.noreply.github.com>
2022-10-22 18:47:16 +0200

    create read_data function

--- source/ex1.py
+++ source/ex1.py
@@ -1,6 +1,18 @@
 import csv
+def read_data(filepath):
+    counter = 0
+    with open(filepath, 'r') as file:
+        csvreader = csv.reader(file)
+        data=[]
+        for row in csvreader:
+            if counter>0:
+                data.append(row)
+            counter=counter+1
+        return data
 
-with open("../Daten/train.csv", 'r') as file:
-  csvreader = csv.reader(file)
-  for row in csvreader:
-    print(row)
+
+train_data=read_data("../Daten/train.csv")
+print(train_data)
+
+ideal_data=read_data("../Daten/ideal.csv")
+print(ideal_data)
